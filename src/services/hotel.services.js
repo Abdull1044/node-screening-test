2604c7f5cbd8b4b5af4aaf3c18aa7225264826ba
Abdullah <abdullah.shafqat@surveyauto.email>
2023-07-09 16:44:06 +0500

    Handle edge cases and user friendly Error Messages

--- src/services/hotel.services.js
+++ src/services/hotel.services.js
@@ -2,6 +2,50 @@ const httpStatus = require("http-status");
 const { Hotel } = require("../models");
 const ApiError = require("../utils/ApiError");
 
+const createHotel = async (newHotel) => {
+  const hotel = await Hotel.create(newHotel);
+  return "New Hotel Added.";
+};
+
+const updateHotel = async (_id, updatedHotel) => {
+  const hotel = await Hotel.findByIdAndUpdate(_id, updatedHotel, {
+    new: true,
+  });
+  if (!hotel) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Hotel not found");
+  }
+  const result = await hotel.save();
+  return result ? "Hotel Info Updated " : "Error While Updating Hotel Info.";
+};
+
+const deleteHotel = async (_id) => {
+  const hotel = await Hotel.findById(_id);
+  if (!hotel) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Hotel not found");
+  }
+  const result = await hotel.deleteOne();
+  if (result) {
+    return "Hotel Info Delete";
+  } else return "Error While Deleting Hotel ";
+};
+
+const findHotelBySlugOrId = async (slugOrId) => {
+  const isObjectId = /^[0-9a-fA-F]{24}$/.test(slugOrId);
+  let query;
+
+  if (isObjectId) {
+    query = { _id: slugOrId };
+  } else {
+    query = { slug: slugOrId };
+  }
+
+  const hotel = await Hotel.findOne(query);
+  if (!hotel) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Failed to fetch found");
+  }
+  return hotel;
+};
+
 const getAll = async (query) => {
   const filters = {};
   const sortOptions = {};
@@ -28,46 +72,6 @@ const getAll = async (query) => {
   const hotels = await Hotel.find(filters).sort(sortOptions);
 
   return { hotels };
-};
-
-const createHotel = async (newHotel) => {
-  const hotel = await Hotel.create(newHotel);
-  return hotel;
-};
-
-const updateHotel = async (_id, updatedHotel) => {
-  const hotel = await Hotel.findByIdAndUpdate(_id, updatedHotel, {
-    new: true,
-  });
-  if (!hotel) {
-    throw new ApiError(httpStatus.NOT_FOUND, "Hotel not found");
-  }
-  return await hotel.save();
-};
-
-const deleteHotel = async (_id) => {
-  const hotel = await Hotel.findById(_id);
-  if (!hotel) {
-    throw new ApiError(httpStatus.NOT_FOUND, "Hotel not found");
-  }
-  return await hotel.deleteOne();
-};
-
-const findHotelBySlugOrId = async (slugOrId) => {
-  const isObjectId = /^[0-9a-fA-F]{24}$/.test(slugOrId);
-  let query;
-
-  if (isObjectId) {
-    query = { _id: slugOrId };
-  } else {
-    query = { slug: slugOrId };
-  }
-
-  const hotel = await Hotel.findOne(query);
-  if (!hotel) {
-    throw new ApiError(httpStatus.NOT_FOUND, "Failed to fetch found");
-  }
-  return hotel;
 };
 
 module.exports = {
